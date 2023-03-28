2b78fb6063ebcdd7b18488424f87ea5180d612f4
vanathi <123821462+Vanathi2694@users.noreply.github.com>
2023-03-28 16:30:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,27 +4,40 @@ import snscrape.modules.twitter as snmtwitter
 from pymongo import MongoClient
 import csv
 
-w = st.text_area("Type word to be scrapped")
+st.write("Twitter Data Scrapping")
+choice = st.selectbox("Pick One",["Keyword", "Hastag"])
+w = st.text_area("Type " + choice + " to be scrapped")
 date = st.date_input("Give the start date the data to be scrapped(Start date should be low or before the End date):")
 date1 = st.date_input("Give the end date the data to be scrapped:")
 choice = st.number_input("Mention the tweetcount to be:")
 click = st.button("Enter")
 
-
+tweets_df1=pd.DataFrame()
 tweet_list = []
 client = MongoClient("mongodb+srv://vanathisoundararajan:vanso2628@cluster0.cdvekaq.mongodb.net/test")
 clientatlas = MongoClient("mongodb+srv://vanathisoundararajan:vanu2628@cluster0.cdvekaq.mongodb.net/?retryWrites=true&w=majority")
 newdb = client["twitterscrapping"]
 newcol = newdb["datas"]
 
-
-for i, tweet in enumerate(snmtwitter.TwitterSearchScraper('w').get_items()):
-    if i > 1000 and date < date1 and choice >= tweet.retweetCount:
-        break
-tweet_list.append([tweet.date, tweet.id, tweet.url, tweet.content, tweet.user, tweet.replyCount, tweet.retweetCount, tweet.lang, tweet.source, tweet.likeCount])
-tweets_df1 = pd.DataFrame(tweet_list, columns=['Datetime', 'Tweetid', 'Url', 'Content', 'User', 'Replycount', 'Retweetcount', 'Language', 'Source', 'Likecount'])
-st.dataframe(tweets_df1)
-
+if w:
+    if choice=="keyword":
+        for i, tweet in enumerate(snmtwitter.TwitterSearchScraper('w').get_items()):
+            if i > 1000 and date < date1 and choice >= tweet.retweetCount:
+                break
+            tweet_list.append([tweet.date, tweet.id, tweet.url, tweet.content, tweet.user, tweet.replyCount, tweet.retweetCount, tweet.lang, tweet.source, tweet.likeCount])
+        tweets_df1 = pd.DataFrame(tweet_list, columns=['Datetime', 'Tweetid', 'Url', 'Content', 'User', 'Replycount', 'Retweetcount', 'Language', 'Source', 'Likecount'])
+        st.dataframe(tweets_df1)
+    else:
+        for i, tweet in enumerate(snmtwitter.TwitterHashtagScraper('w').get_items()):
+            if i > 1000 and date < date1 and choice >= tweet.retweetCount:
+                break
+            tweet_list.append(
+                [tweet.date, tweet.id, tweet.url, tweet.content, tweet.user, tweet.replyCount, tweet.retweetCount, tweet.lang, tweet.source, tweet.likeCount])
+        tweets_df1 = pd.DataFrame(tweet_list, columns=['Datetime', 'Tweetid', 'Url', 'Content', 'User', 'Replycount',
+                                               'Retweetcount', 'Language', 'Source', 'Likecount'])
+        st.dataframe(tweets_df1)
+else:
+    st.warning("Enter the Keyword or Hashtag to be scrapped")
 
 @st.cache_data
 def dwl1(df):
@@ -56,14 +69,18 @@ if 'Download_csv_File' not in st.session_state:
 st.download_button('Download csv File', file_name="python_tweetscrap.csv", data=csv, mime="text/csv")
 st.write(st.session_state.Download_csv_File)
 
-data = st.file_uploader("Upload a CSV")
-
 csvfile = open("python_tweetscrap.csv", 'r')
 dfd = pd.read_csv(csvfile)
 data1 = dfd.to_dict(orient="records")
 newcol.insert_many(data1)
 
 
-print("connection successful")
+def upl():
+    data = st.file_uploader("Upload a CSV")
+    return data
+
+
+st.success("Data scrapped and collected successfully")
+st.snow()
 client.close()
 clientatlas.close()
